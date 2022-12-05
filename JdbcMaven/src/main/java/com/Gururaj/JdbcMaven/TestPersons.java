91dbc57d25316df07d040dac174f8ff88a8af935
Gururajkm <112824187+Gururajkm@users.noreply.github.com>
2022-12-05 16:45:19 +0530

    Update TestPersons.java

--- JdbcMaven/src/main/java/com/Gururaj/JdbcMaven/TestPersons.java
+++ JdbcMaven/src/main/java/com/Gururaj/JdbcMaven/TestPersons.java
@@ -1,12 +1,21 @@
 package com.Gururaj.JdbcMaven;
 
 import com.Gururaj.JdbcMaven.dao.PersonsDao;
+import com.Gururaj.JdbcMaven.entity.Persons;
 
 public class TestPersons {
 	public static void main(String[] args) {
 		
 	PersonsDao dao=new PersonsDao();
 	dao.getAll();
+	Persons persons=new Persons();
+	persons.setId(4);
+	persons.setPname("Virat");
+	persons.setPage(34);
+	
+//	dao.save(persons);
+	
+	dao.update("Yuvarajkm", 1);
 
 }
 }
