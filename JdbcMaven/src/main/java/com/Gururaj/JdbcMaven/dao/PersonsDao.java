65d1b0db6a42207c11eff5635afa93026e4812c5
Gururajkm <112824187+Gururajkm@users.noreply.github.com>
2022-12-05 16:43:50 +0530

    Update PersonsDao.java

--- JdbcMaven/src/main/java/com/Gururaj/JdbcMaven/dao/PersonsDao.java
+++ JdbcMaven/src/main/java/com/Gururaj/JdbcMaven/dao/PersonsDao.java
@@ -3,19 +3,26 @@ package com.Gururaj.JdbcMaven.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.Gururaj.JdbcMaven.entity.Persons;
 
+
 public class PersonsDao {
 	
+	private SessionFactory factory=null;
+	private Object details;
+	
+	public PersonsDao(){
+	Configuration configuration=new Configuration();
+	configuration.configure();
+	SessionFactory factory=configuration.buildSessionFactory();
+	}
+	
+	
 	public void getAll() {
-		
-		Configuration configuration=new Configuration();
-		configuration.configure();
-		SessionFactory factory=configuration.buildSessionFactory();
 		Session session= factory.openSession();
-		
 		Persons persons=session.get(Persons.class,1);
 		System.out.println(persons);
 		Persons persons1=session.get(Persons.class,2);
@@ -24,4 +31,23 @@ public class PersonsDao {
 		session.close();
 	}
 
+	public void save(Persons persons) {
+		Session session= factory.openSession();
+		Transaction transaction=session.beginTransaction();
+		session.save(persons);
+		System.out.println("Data saved");
+		transaction.commit();
+		session.close();
+	}
+	
+	public void update(String pName,int id) {
+		Session session= factory.openSession();
+	    Persons persons=session.get(Persons.class,id);
+	    persons.setPname(pName);
+	    Transaction transaction=session.beginTransaction();
+	
+	    session.save(persons);
+	    transaction.commit();
+		
+	}
 }
